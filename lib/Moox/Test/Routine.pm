4ec8ec152702f45445fdd2516ed68f9ba6db5724
Aaron Moses <zebardy@gmail.com>
2013-02-27 11:29:02 +0000

    Initial commit of code

---
+++ lib/Moox/Test/Routine.pm
@@ -0,0 +1,79 @@
+package Moox::Test::Routine;
+
+use 5.008001;
+use Test::More;
+use base 'Exporter';
+our @EXPORT = qw(test run_me run_test);
+
+my $test_cache;
+our $VERSION = 0.0.1;
+
+sub test {
+  my $name = shift;
+  my $code = pop;
+
+  $test_cache->{$name} = $code;
+};
+
+sub run_me {
+  my $class = scalar(caller);
+  my $instance = $class->new();
+  foreach my $test_name (keys %$test_cache) {
+    $instance->run_test($test_name, $test_cache->{$test_name});
+  }
+};
+
+sub run_test {
+  my ($self, $name, $code) = @_;
+
+  Test::More::subtest($name, sub { &{$code}($self) });
+}
+
+=pod
+
+=head1 NAME
+
+Moox::Test::Routine - A simple test framework for Moo which provides a slice of the functionality of Test::Routine without the complexity of Moose
+
+=head1 SYNOPSIS
+
+  use Moo;
+  use Test::More;
+
+  use Moox::Test::Routine;
+
+  has foo => ( is => 'rw', default => sub { 'bar' } );
+
+  test "bob test" => sub {
+      my $self = shift;
+      print "running test\n";
+      ok 1;
+      is $self->foo, 'bar';
+  };
+
+  before run_test => sub {
+      print "running before each test!\n";
+  };
+
+  run_me;
+  done_testing;
+  1;
+
+=head1 DESCRIPTION
+
+This module provides enough of the Test::Routine look and feel to make nice tests when using Moo
+
+=head1 AUTHOR
+
+Aaron Moses E<lt>zebardy@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2013 Aaron Moses.
+
+This program is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
